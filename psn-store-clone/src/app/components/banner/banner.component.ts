73a46fe6e3817b9fd12f963986e0fb3b92cf6ffe
Pablo de Brito Carvalho <pablo_pbc@outlook.com>
2023-08-29 18:12:18 -0300

    Feat: banner component added

--- psn-store-clone/src/app/components/banner/banner.component.ts
+++ psn-store-clone/src/app/components/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent {
+  @Input()
+  src: string = '';
+  @Input()
+  descrition: string = '';
+  @Input()
+  display: string = '';
+  @Input()
+  title: string = '';
+  @Input()
+  text: string = '';
 
+  
 }
