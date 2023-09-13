1b7c49e331a40f64c27abe82973997925a77da61
Pablo de Brito Carvalho <pablo_pbc@outlook.com>
2023-09-13 17:41:24 -0300

    feat: adding footer component

--- Angular/psn-store-clone/src/app/components/footer/footer.component.ts
+++ Angular/psn-store-clone/src/app/components/footer/footer.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  sac: string[];
+  politics: string[];
+  social: string[];
 
+  constructor() {
+    this.sac = [
+      'Suporte',
+      'Política de privacidade',
+      'Termos de uso do site',
+      'Mapa do site',
+      'PlayStation Studios',
+      'Legal',
+      'Sobre a SIE',
+    ];
+
+    this.politics = [
+      'Termos de serviço da PSN',
+      'Política de cancelamento da PS Store',
+      'Avisos de saúde',
+      'Sobre as classificações',
+    ];
+
+    this.social = [
+      'Facebook',
+      'Twitter',
+      'YouTube',
+      'Instagram',
+      'Aplicativo para Android',
+      'Aplicativo para iOS',
+    ];
+  }
 }
